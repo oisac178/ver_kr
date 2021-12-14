11db6f1363d34a1379ce706f0d5b4cb3e59d8f5f
Alex <pf775_ash@rambler.ru>
2021-12-14 22:31:53 +0300

    galery

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,34 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".header__down-btn").forEach(item => {
-  item.addEventListener("click", function() {
-    let btn = this;
-    let dropdown = this.parentElement.querySelector(".header__dropdown");
+    item.addEventListener("click", function () {
+      let btn = this;
+      let dropdown = this.parentElement.querySelector(".header__dropdown");
 
-    document.querySelectorAll(".header__down-btn").forEach(el => {
-      if (el != btn) {
-        el.classList.remove("active--btn");
-      }
-    });
+      document.querySelectorAll(".header__down-btn").forEach(el => {
+        if (el != btn) {
+          el.classList.remove("active--btn");
+        }
+      });
 
-    document.querySelectorAll(".header__dropdown").forEach(el => {
-      if (el != dropdown) {
-        el.classList.remove("active-list--item");
-      }
+      document.querySelectorAll(".header__dropdown").forEach(el => {
+        if (el != dropdown) {
+          el.classList.remove("active-list--item");
+        }
+      })
+      dropdown.classList.toggle("active-list--item");
+      btn.classList.toggle("active--btn")
     })
-    dropdown.classList.toggle("active-list--item");
-    btn.classList.toggle("active--btn")
   })
-})
 
-document.addEventListener("click", function(e) {
-  let target = e.target;
-  if (!target.closest(".header__down-list")) {
-    document.querySelectorAll(".header__dropdown").forEach(el => {
+  document.addEventListener("click", function (e) {
+    let target = e.target;
+    if (!target.closest(".header__down-list")) {
+      document.querySelectorAll(".header__dropdown").forEach(el => {
         el.classList.remove("active-list--item");
-    })
-     document.querySelectorAll(".header__down-btn").forEach(el => {
+      })
+      document.querySelectorAll(".header__down-btn").forEach(el => {
         el.classList.remove("active--btn");
-    });
-  }
-})
+      });
+    }
+  })
+
+  document.querySelectorAll('.header__down-btn').addEventListener('click', function () {
+    document.querySelector('#btn-down').classList.toggle('is-show')
+  })
 })
