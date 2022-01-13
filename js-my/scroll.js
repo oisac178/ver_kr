03fba5f207eb855ab048dbae5d4d3fd8be820f24
Alex <pf775_ash@rambler.ru>
2022-01-13 22:48:21 +0300

    checkbox+accordeon

--- js-my/scroll.js
+++ js-my/scroll.js
@@ -6,11 +6,21 @@ $(document).ready(function(){
       $('body,html').animate({scrollTop: top}, 1500);
   });
 });
+
 $(document).ready(function(){
   $("#sighup").on("click","a", function (event) {
       event.preventDefault();
       var id  = $(this).attr('href'),
           top = $(id).offset().top;
       $('body,html').animate({scrollTop: top}, 2500);
+  });
+});
+
+$(document).ready(function(){
+  $("#pintor").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
   });
 });
--- js-my/tabs.js
+++ js-my/tabs.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.section-how__btn').forEach(function(tabsBtn) {
+  document.querySelectorAll('.section-catalog__data-btn').forEach(function(tabsBtn) {
     tabsBtn.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
       document.querySelectorAll('.tab-content').forEach(function(tabContent) {
         tabContent.classList.remove('tab-content-active')
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
-
-      document.querySelectorAll('.section-how__btn').forEach(function(tabBtn) {
-        tabBtn.classList.remove('section-how__btn-active')
-      })
-      document.querySelector(`[data-path="${path}"]`).classList.add('section-how__btn-active')
     })
   })
 })
